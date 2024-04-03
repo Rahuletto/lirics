9061175ce38fc1e4a056539b6bd2f10816b075cc
Rahul Marban <rahulmarban@gmail.com>
2024-04-03 07:29:26 +0530

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -40,7 +40,7 @@ export default function Home() {
       var parentRect = parent.getBoundingClientRect();
       var childRect = child.getBoundingClientRect();
 
-      parent.scrollTop +=
+      parent.scrollTop += 30 +
         childRect.top - parentRect.top - parent.clientHeight / 2;
     }
   }
