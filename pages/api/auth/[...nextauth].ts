5813ebd82577791f0492355062e076422078dc4b
Rahul Marban <rahulmarban@gmail.com>
2024-06-12 05:58:14 +0000

    feat: using refresh token to make it easy to auth

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,6 +1,23 @@
-import NextAuth from "next-auth";
+import NextAuth, { User } from "next-auth";
 import spotifyApi from "../../../lib/spotify";
 import Spotify from "next-auth/providers/spotify";
+import { JWT } from "next-auth/jwt";
+
+declare module "next-auth" {
+  interface Session {
+    error?: "RefreshAccessTokenError";
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    access_token: string;
+    expires_at: number;
+    refresh_token: string;
+    error?: "RefreshAccessTokenError";
+  }
+}
+
 export default NextAuth({
   providers: [
     Spotify({
@@ -8,13 +25,19 @@ export default NextAuth({
       clientSecret: process.env.SECRET as string,
       authorization: {
         params: {
-          scope:
-            "user-read-playback-state user-read-currently-playing",
+          scope: "user-read-playback-state user-read-currently-playing",
         },
       },
     }),
   ],
   callbacks: {
+    async session({ session, token }) {
+      if (token.user) {
+        session.user = token.user as User;
+      }
+
+      return session;
+    },
     async signIn({ user, account, profile }) {
       if (account && account.provider === "spotify") {
         spotifyApi.setAccessToken(account?.access_token as string);
@@ -22,14 +45,48 @@ export default NextAuth({
         return true;
       } else return false;
     },
-    jwt: async ({ token, account }: any) => {
+    async jwt({ token, account }) {
       if (account) {
         token.access_token = account.access_token as string;
+        token.expires_at = account.expires_at as number;
+        token.refresh_token = account.refresh_token as string;
+
+        spotifyApi.setAccessToken(account?.access_token as string);
+
+        return {
+          access_token: account.access_token as string,
+          expires_at: account.expires_at as number,
+          refresh_token: account.refresh_token as string,
+        };
+      } else {
+        const isTokenExpired = Date.now() >= token.expires_at * 1000;
+
+        if (isTokenExpired) {
+          if (!token.refresh_token) throw new Error("Missing refresh token");
+          try {
+            const response = await spotifyApi.refreshAccessToken();
+            const responseTokens = response.body;
+
+            return {
+              ...token,
+              access_token: responseTokens.access_token,
+              expires_at: Math.floor(
+                Date.now() / 1000 + (responseTokens.expires_in as number)
+              ),
+              refresh_token:
+                responseTokens.refresh_token ?? token.refresh_token,
+            } as JWT;
+          } catch (error) {
+            console.error("Error refreshing access token", error);
+            return {
+              ...token,
+              error: "RefreshAccessTokenError" as const,
+            } as JWT;
+          }
+        } else {
+          return token as JWT;
+        }
       }
-
-      spotifyApi.setAccessToken(account?.access_token as string);
-
-      return token;
     },
   },
   pages: {
